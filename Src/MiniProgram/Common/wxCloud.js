42f3ed72f4e2aaa035e2c68411d5debe3a5d0d10
王占领 <wangzl1163@qq.com>
2019-09-07 16:55:55 +0800

    去除回调函数支持

--- Src/MiniProgram/Common/wxCloud.js
+++ Src/MiniProgram/Common/wxCloud.js
@@ -1,28 +1,34 @@
 // 云开发 小程序端API封装
 
+/**
+ * 全部函数返回Promise，不支持回调函数风格
+ */
+
 class WxCloud {
    /**
     * 调用云函数
+    * @Params {object} {name,data}
+    * {
+    *   name:云函数名,
+    *   data:传递给云函数的参数
+    * }
+    * 
+    * @params {object} config 环境配置
     */
    callFunction({
       name,
-      data,
-      success,
-      fail,
-      complete
+      data
    }, config = wx.cloud_env) {
       return wx.cloud.callFunction({
          name,
          data,
-         config,
-         success,
-         fail,
-         complete
+         config
       })
    }
 
    /**
     * 获取数据库对象
+    * @params {object} config 环境配置
     */
    getDB(config = wx.cloud_env) {
       return wx.cloud.database(config)
@@ -30,6 +36,7 @@ class WxCloud {
 
    /**
     * 获取集合对象
+    * @param {string} collectionName 集合名称
     */
    getCollection(collectionName, dbConfig = wx.cloud_env) {
       return this.getDB(dbConfig).collection(collectionName)
@@ -37,6 +44,8 @@ class WxCloud {
 
    /**
     * 获取记录对象
+    * @params {string} collectionName 集合名称
+    * @params {string} docId 记录ID
     */
    getDoc(collectionName, docId, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).doc(docId)
@@ -44,38 +53,30 @@ class WxCloud {
 
    /**
     * 在集合上新增记录
+    * @params {string} collectionName 集合名称
+    * @params {object} data 新增记录的定义
+    * @params {object} dbConfig 环境配置
     */
-   add(collectionName, {
-      data = {},
-      success,
-      fail,
-      complete
-   } = {}, dbConfig = wx.cloud_env) {
+   add(collectionName, data = {}, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).add({
-         data,
-         success,
-         fail,
-         complete
+         data
       })
    }
 
    /**
     * 获取集合数据，或获取根据查询条件筛选后的集合数据。
+    * @params {string} collectionName 集合名称
+    * @params {object} dbConfig 环境配置
     */
-   get(collectionName, {
-      success,
-      fail,
-      complete
-   }, dbConfig = wx.cloud_env) {
-      return this.getCollection(collectionName, dbConfig).get({
-         success,
-         fail,
-         complete
-      })
+   get(collectionName, dbConfig = wx.cloud_env) {
+      return this.getCollection(collectionName, dbConfig).get()
    }
 
    /**
     * 指定筛选条件
+    * @params {string} collectionName 集合名称
+    * @params {object} rule 筛选条件
+    * @params {object} dbConfig 环境配置
     */
    where(collectionName, rule = {}, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).where(rule)
@@ -83,6 +84,10 @@ class WxCloud {
 
    /**
     * 指定查询排序条件
+    * @params {string} collectionName 集合名称
+    * @params {string} fieldName 排序字段
+    * @params {string} order 排序顺序，只能为 asc（升序） 或 desc（降序）
+    * @params {object} dbConfig 环境配置
     */
    orderBy(collectionName, fieldName, order, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).orderBy(fieldName, order)
@@ -90,6 +95,9 @@ class WxCloud {
 
    /**
     * 指定查询结果集数量上限
+    * @params {string} collectionName 集合名称
+    * @params {number} max 查询结果集的数量上限
+    * @params {object} dbConfig 环境配置
     */
    limit(collectionName, max, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).limit(max)
@@ -97,6 +105,9 @@ class WxCloud {
 
    /**
     * 指定查询返回结果时从指定序列后的结果开始返回，常用于分页
+    * @params {string} collectionName 集合名称
+    * @params {number} offset 跳过的数量
+    * @params {object} dbConfig 环境配置
     */
    skip(collectionName, offset, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).skip(offset)
@@ -104,6 +115,9 @@ class WxCloud {
 
    /**
     * 指定返回结果中记录需返回的字段
+    * @params {string} collectionName 集合名称
+    * @params {object} definition 返回结果中记录需返回的字段
+    * @params {object} dbConfig 环境配置
     */
    field(collectionName, definition = {}, dbConfig = wx.cloud_env) {
       return this.getCollection(collectionName, dbConfig).field(definition)
@@ -111,6 +125,9 @@ class WxCloud {
 
    /**
     * 获取记录数据，或获取根据查询条件筛选后的记录数据
+    * @params {string} collectionName 集合名称
+    * @params {string} docId 记录ID
+    * @params {object} dbConfig 环境配置
     */
    docGet(collectionName, docId, dbConfig = wx.cloud_env) {
       return this.getDoc(collectionName, docId, dbConfig).get()
@@ -118,53 +135,43 @@ class WxCloud {
 
    /**
     * 更新一条记录
+    * @params {string} collectionName 集合名称
+    * @params {string} docId 记录ID
+    * @params {object} data 更新对象
+    * @params {object} dbConfig 环境配置
     */
-   update(collectionName, docId, data, {
-      success,
-      fail,
-      complete
-   } = {}, dbConfig = wx.cloud_env) {
+   update(collectionName, docId, data, dbConfig = wx.cloud_env) {
       return this.getDoc(collectionName, docId, dbConfig).update({
-         data,
-         success,
-         fail,
-         complete
+         data
       })
    }
 
    /**
     * 替换更新一条记录
+    * @params {string} collectionName 集合名称
+    * @params {string} docId 记录ID
+    * @params {object} data 替换更新对象
+    * @params {object} dbConfig 环境配置
     */
-   set(collectionName, docId, data, {
-      success,
-      fail,
-      complete
-   } = {}, dbConfig = wx.cloud_env) {
+   set(collectionName, docId, data, dbConfig = wx.cloud_env) {
       return this.getDoc(collectionName, docId, dbConfig).set({
-         data,
-         success,
-         fail,
-         complete
+         data
       })
    }
 
    /**
     * 删除一条记录
+    * @params {string} collectionName 集合名称
+    * @params {string} docId 记录ID
+    * @params {object} dbConfig 环境配置
     */
-   remove(collectionName, docId, {
-      success,
-      fail,
-      complete
-   } = {}, dbConfig = wx.cloud_env) {
-      return this.getDoc(collectionName, docId, dbConfig).remove({
-         success,
-         fail,
-         complete
-      })
+   remove(collectionName, docId, dbConfig = wx.cloud_env) {
+      return this.getDoc(collectionName, docId, dbConfig).remove()
    }
 
    /**
     * 获取数据库查询及更新指令
+    * @params {object} dbConfig 环境配置
     */
    command(config = wx.cloud_env) {
       return this.getDB(config).command
@@ -172,6 +179,7 @@ class WxCloud {
 
    /**
     * 数据库正则
+    * @params {object} dbConfig 环境配置
     */
    getDbRegExp(config = wx.cloud_env) {
       return this.getDB(config).RegExp
@@ -179,6 +187,11 @@ class WxCloud {
 
    /**
     * 构造一个服务端时间的引用。可用于查询条件、更新字段值或新增记录时的字段值。
+    * @params {object} 
+    * {
+    *    offset 引用的服务端时间偏移量，毫秒为单位，可以是正数或负数
+    * }
+    * @params {object} dbConfig 环境配置
     */
    serverDate({
       offset
@@ -190,6 +203,7 @@ class WxCloud {
 
    /**
     * 地理位置构造器
+    * @params {object} dbConfig 环境配置
     */
    getGeo(config = wx.cloud_env) {
       return this.getDB(config).Geo
@@ -197,13 +211,16 @@ class WxCloud {
 
    /**
     * 聚合流水线阶段
+    * @params {string} collectionName 集合名称
+    * @params {object} dbConfig 环境配置
     */
    aggregationStages(collectionName, config = wx.cloud_env) {
-      return this.getCollection(collectionName, config = wx.cloud_env).aggregate()
+      return this.getCollection(collectionName, config).aggregate()
    }
 
    /**
     * 聚合操作符
+    * @params {object} dbConfig 环境配置
     */
    aggregationOperators(config = wx.cloud_env) {
       return this.command(config).aggregate
@@ -211,81 +228,75 @@ class WxCloud {
 
    /**
     * 上传文件，将本地资源上传至云存储空间，如果上传至同一路径则是覆盖写
+    * @params {object} {
+    *    cloudPath：云存储路径，命名限制见文件名命名限制
+    *    filePath：要上传文件资源的路径
+    *    header：HTTP 请求 Header, header 中不能设置 Referer
+    *    config：环境配置
+    * }
     */
    uploadFile({
       cloudPath = '',
       filePath = '',
       header = {},
-      config = wx.cloud_env,
-      success,
-      fail,
-      complete,
+      config = wx.cloud_env
    }) {
       return wx.cloud.uploadFile({
          cloudPath,
          filePath,
          header,
-         config,
-         success,
-         fail,
-         complete
+         config
       })
    }
 
    /**
     * 下载文件，从云存储空间下载文件
+    * @params {object} {
+    *    fileID：云文件 ID
+    *    config：环境配置
+    * }
     */
    downloadFile({
       fileID = '',
-      config = wx.cloud_env,
-      success,
-      fail,
-      complete
+      config = wx.cloud_env
    }) {
       return wx.cloud.downloadFile({
          fileID,
-         config,
-         success,
-         fail,
-         complete
+         config
       })
    }
 
    /**
     * 用云文件 ID 换取真实链接，可自定义有效期，默认一天且最大不超过一天。一次最多取 50 个。
+    * @params {object} {
+    *    fileList：要换取临时链接的云文件 ID 列表
+    *    config：环境配置
+    * }
     */
    getTempFileURL({
       fileList: [],
-      config = wx.cloud_env,
-      success,
-      fail,
-      complete
+      config = wx.cloud_env
    }) {
       return wx.cloud.getTempFileURL({
          fileList,
-         config,
-         success,
-         fail,
-         complete
+         config
       })
    }
 
    /**
     * 从云存储空间删除文件，一次最多 50 个
+    * @params {object} {
+    *    fileList：云文件 ID 字符串数组
+    *    config：环境配置
+    * }
     */
    deleteFile({
       fileList: [],
-      config = wx.cloud_env,
-      success,
-      fail,
-      complete
+      config = wx.cloud_env
    }) {
       return wx.cloud.deleteFile({
          fileList,
-         config,
-         success,
-         fail,
-         complete
+         config
       })
    }
 }
