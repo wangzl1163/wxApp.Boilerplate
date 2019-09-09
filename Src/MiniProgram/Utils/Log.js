0df0790ba3ba684630f40bff8774402b394f22fe
王占领 <wangzl1163@qq.com>
2019-09-09 13:30:24 +0800

    优化日志代码

--- Src/MiniProgram/Utils/Log.js
+++ Src/MiniProgram/Utils/Log.js
@@ -1,9 +1,11 @@
 const tool = require('/Tools.js');
 const logger = wx.canIUse('getLogManager') ? wx.getLogManager() : null;
 
-const log = function(title, msg) {
+const log = function (title, msg) {
    if (logger && !wx.app_env.isProduction) {
-      logger.log('时间：' + tool.sampleFormatTime(new Date(Date.now())) + (title || '小程序逻辑层发生异常') + '-----异常信息：', msg);
+      let summary = '时间：' + tool.sampleFormatTime(new Date(Date.now())) + (title || '小程序逻辑层发生异常') + '-----异常信息：'
+
+      logger.log(summary, msg);
    }
 }
 
