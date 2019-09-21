638521c220b93b5cc5cee9e763e722cd6f50f59a
王占领 <wangzl1163@qq.com>
2019-09-21 14:31:54 +0800

    绑定page实例的data和setData

--- Src/Service/BaseService.js
+++ Src/Service/BaseService.js
@@ -1,7 +1,11 @@
 class Service {
-   constructor(){
+   constructor() {
       let pages = getCurrentPages()
-      this.page = pages[pages.length-1]
+      this.page = pages[pages.length - 1]
+      this.$setData = (data, cb = () => {}) => {
+         return this.page.setData(data, cb)
+      }
+      this.$data = this.page.data
    }
 }
 
