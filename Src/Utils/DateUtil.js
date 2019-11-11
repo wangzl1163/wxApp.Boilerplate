6c0cec1777ccc428357445abb37102f65100b213
王占领 <wangzl1163@qq.com>
2019-11-11 11:03:49 +0800

    支持标准时间日期字符串

--- Src/Utils/DateUtil.js
+++ Src/Utils/DateUtil.js
@@ -1,9 +1,13 @@
 /**
  * 日期时间格式化
- * @params {Date} date Date对象
- * @params {string} fmt 格式
+ * @params {string|Date} date 标准日期时间字符串或Date对象
+ * @params {string} fmt 日期时间格式
  */
-const dateTimeFormate = function(date, fmt) {
+const dateTimeFormate = (date, fmt) => {
+   if (typeof date === 'string') {
+      date = new Date(date)
+   }
+
    var o = {
       "M+": date.getMonth() + 1, //月份   
       "d+": date.getDate(), //日   
@@ -14,11 +18,11 @@ const dateTimeFormate = function(date, fmt) {
       "S": date.getMilliseconds() //毫秒   
    }
 
-   if (/(y+)/.test(fmt)){
+   if (/(y+)/.test(fmt)) {
       fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length))
    }
-      
-   for (var k in o){
+
+   for (var k in o) {
       if (new RegExp("(" + k + ")").test(fmt)) {
          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)))
       }
@@ -29,9 +33,14 @@ const dateTimeFormate = function(date, fmt) {
 
 /**
  * 时间日期简单格式化 yyyy/MM/dd hh:mm:ss
- * @params {Date} date Date对象
+ * @params {Date|string} date Date对象或标准日期时间字符串
+ * @params {string} connector 年月日间的连接符，默认为“/”
  */
-const sampleFormatTime = date => {
+const sampleFormatTime = (date, connector = '/') => {
+   if (typeof date === 'string') {
+      date = new Date(date)
+   }
+
    const year = date.getFullYear()
    const month = date.getMonth() + 1
    const day = date.getDate()
@@ -39,7 +48,7 @@ const sampleFormatTime = date => {
    const minute = date.getMinutes()
    const second = date.getSeconds()
 
-   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+   return [year, month, day].map(formatNumber).join(connector) + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 
