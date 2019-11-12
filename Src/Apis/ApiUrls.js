511381fe942892cfdf9a890bbef83daebd71e138
王占领 <wangzl1163@qq.com>
2019-11-12 18:25:56 +0800

    规范命名，优化逻辑

--- Src/Apis/ApiUrls.js
+++ Src/Apis/ApiUrls.js
@@ -1,11 +1,19 @@
 const domains = require('./Domain.js')
-console.log('小程序当前域名：',domains)
+console.log('小程序当前域名：', domains)
 
-const apis = {
-   userLogin: domains.apiDomain + '/api/User/UserLogin', // 用户登录
+const urls = {
+   userLogin: 'User/UserLogin', // 用户登录
+}
+
+const getUrls = () => {
+   for (const key in urls) {
+      urls[key] = domains.apiDomain + '/api/' + urls[key]
+   }
+
+   return urls
 }
 
 module.exports = {
-   apis,
+   urls: getUrls(),
    domains
 }
