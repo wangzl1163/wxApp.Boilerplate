6946d33b8d76d5c401cde69cea3f5ac76ead7318
王占领 <wangzl1163@qq.com>
2020-06-02 17:24:42 +0800

    增加纯数据字段规则

--- Src/Extend/Component.js
+++ Src/Extend/Component.js
@@ -5,14 +5,16 @@ class C {
    // 全局Behaviors
    behaviors = []
 
-   //组件指定了behavior则合并全局behavior与组件的behavior
-   // 合并顺序为全局的在前，组件的在后，以遵循小程序Behavior属性和数据及方法合并规则
-   // 
-   // 组件未指定behavior时则加上全局behavior
-   create(option) {
-      option.behaviors = option.behaviors ? [...this.behaviors, ...option.behaviors] : [...this.behaviors]
+   create(options) {
+      // 指定组件的纯数据字段的正则表达式
+      options.options.pureDataPattern = options.options.pureDataPattern ? options.options.pureDataPattern : /^_/ // 指定所有 _ 开头的数据字段为纯数据字段
 
-      return Component(option)
+      // 组件指定了behavior则合并全局behavior与组件的behavior
+      // 合并顺序为全局的在前，组件的在后，以遵循小程序Behavior属性和数据及方法合并规则
+      // 组件未指定behavior时则加上全局behavior
+      options.behaviors = options.behaviors ? [...this.behaviors, ...options.behaviors] : [...this.behaviors]
+
+      return Component(options)
    }
 }
 
